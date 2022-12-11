93622eb70213b2cb26721b352d4368630fc12d82
EdgarGrcs <edgar.gricius2@gmail.com>
2022-12-11 23:26:24 +0100

    add createNavbar module

--- src/landing.js
+++ src/landing.js
@@ -6,36 +6,42 @@ function createNavBar() {
     const aHome = document.createElement("div");
     aHome.classList.add("nav-text");
     aHome.textContent = "Home";
+    const navRight = document.createElement("div");
+    navRight.classList.add("nav-right");
+    const menuDiv = document.createElement("div");
+    menuDiv.classList.add("nav-text");
+    menuDiv.textContent = "Menu";
+    const aboutDiv = document.createElement("div");
+    aboutDiv.classList.add("nav-text");
+    aboutDiv.textContent = "About";
+
+    const contactDiv = document.createElement("div");
+    contactDiv.classList.add("nav-text");
+    contactDiv.textContent = "Content";
+
+    navRight.appendChild(menuDiv);
+    navRight.appendChild(aboutDiv);
+    navRight.appendChild(contactDiv);
 
     navDiv.appendChild(aHome);
-
-
+    navDiv.appendChild(navRight);
 
 
     //navDiv ready to append to content
-
-
-
     return navDiv;
 }
 
-
-function backgroundImage() {
-
+/* function backgroundImage() {
     let backgroundImageDiv = document.createElement("div");
     backgroundImageDiv.classList.add("background-image");
-
-
     return backgroundImageDiv;
-}
-
-
+} */
 
 
 function initWebsite() {
     const content = document.getElementById("content");
     content.appendChild(createNavBar());
-    content.appendChild(backgroundImage());
+    //  content.appendChild(backgroundImage());
 }
 
 export default initWebsite;
